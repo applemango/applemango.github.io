04063979802f70ddc514fae5cb4cd81504784401
applemango <apple.mangoooo97@gmail.com>
2022-05-13 18:13:52 +0900

    2022/05/13

---
+++ m.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.querySelector("head").innerHTML = `
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="s.css">
+    <title>Hello</title>
+    `;
+}
