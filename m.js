7bb8d02eb6e8b278d461fe373198d6d3d8fc7e90
applemango <apple.mangoooo97@gmail.com>
2022-07-11 07:41:38 +0900

    fix meta

--- m.js
+++ m.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     document.querySelector("head").innerHTML = `
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description content="applemango's profile">
     <link rel="stylesheet" href="s.css">
     <title>Hello</title>
     `;
