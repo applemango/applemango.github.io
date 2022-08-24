029c2887d9c94ecb07711a8228bc4729bea94439
applemango <apple.mangoooo97@gmail.com>
2022-08-24 12:48:35 +0900

    fix domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.GITHUB_ACTIONS && "/git.abc.osaka"
+  basePath: process.env.GITHUB_ACTIONS && "/"
   ,trailingSlash: true
 
   ,reactStrictMode: true
