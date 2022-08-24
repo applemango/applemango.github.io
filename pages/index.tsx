50ad1f2674b541686815f4a4925324b4944f864d
applemango <apple.mangoooo97@gmail.com>
2022-08-24 12:31:43 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import styles from "./styles/index.module.scss"
+
+const Home: NextPage = () => {
+    return (
+        <div className={ styles.main }>
+            <h1>Hello, world!</h1>
+        </div>
+    )
+}
+
+export default Home
