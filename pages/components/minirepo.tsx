ae1e6f075a2c4ec8bbc25ce473d145433effc53c
applemango <apple.mangoooo97@gmail.com>
2022-08-26 06:40:54 +0900

    fix font

---
+++ pages/components/minirepo.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { RepoIcon } from '@primer/octicons-react'
+
+import { getColor } from "../../lib/languagesColor"
+
+import styles from "./styles/minirepo.module.scss"
+
+type Props = {
+    title: string;
+    description: string
+    language: string
+    className?: any
+    style?: any
+}
+const MiniRepo = ({ title, description, language, className, style }:Props) => {
+    return (
+        <div className={` ${styles.main} ${className} `} style={style}>
+            <div className={ styles.title }>
+                <RepoIcon size={16} />
+                <a href={`https://github.com/applemango/${title}`} className={ styles.title_ }>{title}</a>
+                <div className={ styles.public }>
+                    <p>public</p>
+                </div>
+            </div>
+            <div className={ styles.description }>
+                <p>{description}</p>
+            </div>
+            <div className={ styles.lang }>
+                <div className={ styles.color } style={{
+                    backgroundColor: getColor(language)
+                }} />
+                <p>{language}</p>
+            </div>
+        </div>
+    )
+}
+export default MiniRepo
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,54 +2,57 @@ import type { NextPage } from 'next'
 
 import Heads from "./components/heads"
 import Languages from "./components/languages"
+import MiniRepo from "./components/minirepo"
 
 import styles from "./styles/index.module.scss"
 import animations from "./components/styles/animations.module.scss"
 
 const Home: NextPage = () => {
     return (
-        <div className={ styles.main }>
+        <div className={ styles.flex }>
             <Heads title={"AppleMango"} description={"AppleMango - 主にNext.jsやFlaskを使う色々やりたいと思ってる人"} />
-            <div className={styles.main_}>
-                <p className={styles.title_path}><a className={`${animations.underline} ${animations.simple}`} href="https://github.com/applemango">applemango</a> / README.md</p>
-                <h1>Hello! I&apos;m AppleMango</h1>
-                <p>色々, 最近は主にNext.jsやFlaskを使ってWeb Appを作っています</p>
-                <p>稀にMLやDLをしている</p>
-                <div className={styles.langs}>
-                    <Languages
-                        title={"Most Used Languages"}
-                        className={styles.lang}
-                        style={{
-                            width: '60%'
-                            ,marginRight: 15
-                        }}
-                        data={
-                            {
-                                "TypeScript":25
-                                ,"Python":20
-                                ,"JavaScript":15
-                                ,"SCSS":15
-                                ,"CSS":5
-                                ,"Other":20
+            <div className={ styles.main }>
+                <div className={`${styles.main_} ${styles.section}`}>
+                    <p className={styles.title_path}><a className={`${animations.underline} ${animations.simple}`} href="https://github.com/applemango">applemango</a> / README.md</p>
+                    <h1>Hello! I&apos;m AppleMango</h1>
+                    <p>色々, 最近は主にNext.jsやFlaskを使ってWeb Appを作っています</p>
+                    <p>稀にMLやDLをしている</p>
+                    <div className={styles.langs}>
+                        <Languages
+                            title={"Most Used Languages"}
+                            className={styles.lang}
+                            style={{
+                                width: '60%'
+                                ,marginRight: 15
+                            }}
+                            data={
+                                {
+                                    "TypeScript":25
+                                    ,"Python":20
+                                    ,"JavaScript":15
+                                    ,"SCSS":15
+                                    ,"CSS":5
+                                    ,"Other":20
+                                }
                             }
-                        }
-                    />
-                    <Languages
-                        title={"Most Used Framework"}
-                        link={false}
-                        className={styles.lang}
-                        style={{
-                            width: '40%'
-                        }}
-                        data={
-                            {
-                                "Next.js":[70,"#2c3e50"]
-                                ,"Flask":[15,"#3572A5"]
-                                ,"React":[5,"#3178c6"]
-                                ,"Other":10
+                        />
+                        <Languages
+                            title={"Most Used Framework"}
+                            link={false}
+                            className={styles.lang}
+                            style={{
+                                width: '40%'
+                            }}
+                            data={
+                                {
+                                    "Next.js":[70,"#2c3e50"]
+                                    ,"Flask":[15,"#3572A5"]
+                                    ,"React":[5,"#3178c6"]
+                                    ,"Other":10
+                                }
                             }
-                        }
-                    />
+                        />
+                    </div>
                 </div>
             </div>
         </div>
