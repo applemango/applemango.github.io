afcb9daa3ab505f765e464b1800663a8a8bac6a1
applemango <applemango@users.noreply.github.com>
2023-02-16 15:04:06 +0000

    deploy: 5b2352f0a3b3ba3c4b1961fadf865e657e538501

--- _next/static/O2BOwjGZUlxQ9wq2IIRP_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0e226fb0-5ef6a220f7118058.js","static/css/56973ed990c32da1.css","static/chunks/pages/index-424fb27eaebd3610.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
