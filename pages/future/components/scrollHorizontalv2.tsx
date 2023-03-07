ba8d2f0bd1106351720bb471cdc5720ef2bce571
apple <apple.mangoooo97@gmail.com>
2023-03-07 16:58:06 +0900

    add horizontal scroll

---
+++ pages/future/components/scrollHorizontalv2.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useRef, useState } from "react";
+import useScrollPosition from "../../../components/hook/useScrollPosition";
+import useWindowSize from "../../../components/hook/useWindowSize";
+
+type element = React.ReactComponentElement<any>
+type elements = Array<element>
+
+const ScrollHorizontal = ({children}:{
+    children: element | elements
+}) => {
+    const scrollPosition = useScrollPosition();
+    const [elementWidth, setElementWidth] = useState(0)
+    const [elementTop, setElementTop] = useState(0)
+    const [horizontal, setHorizontal] = useState(false)
+    const [elementTransformX, setElementTransformX] = useState(0)
+    const [end, setEnd] = useState(false)
+    const [elementEndPositionY, setElementEndPositionY] = useState(0)
+    const [elementEndPositionX, setElementEndPositionX] = useState(0)
+    const windowSize = useWindowSize();
+    const ref = useRef<any>(null)
+    useEffect(() => {
+        if(!process.browser)
+            return
+        window.scrollTo({
+            top: 0
+        })
+    }, [windowSize])
+    useEffect(()=> {
+        if(!ref?.current)
+            return
+        const h = window.innerHeight
+        const w = window.innerWidth
+        const x = w - h
+        setElementWidth(ref.current.offsetWidth)
+        setElementTop(ref.current.getBoundingClientRect().top)
+    },[ref, windowSize])
+    useEffect(()=> {
+        const h = window.innerHeight
+        const w = window.innerWidth
+        const x = w - h
+        setHorizontal(false)
+        setEnd(false)
+        if(elementTop > scrollPosition)
+            return
+        
+        if(elementTop + elementWidth < scrollPosition + h + x)
+            return setEnd(true)
+
+        const pnr = elementTop + elementWidth - scrollPosition
+        setHorizontal(true)
+
+        setElementTransformX(elementWidth - pnr)
+    },[scrollPosition, elementWidth, elementTop, windowSize])
+    useEffect(()=> {
+        if(!ref?.current)
+            return
+        const h = window.innerHeight
+        const w = window.innerWidth
+        const x = w - h
+        setElementEndPositionY(elementWidth - window.innerHeight - x)
+        setElementEndPositionX(elementWidth - window.innerWidth)
+    },[scrollPosition, elementWidth, elementTop, windowSize])
+    return <div ref={ref} style={{
+        height: process.browser ? elementWidth - (window.innerWidth - window.innerHeight) : elementWidth,
+        width: "fit-content",
+    }}>
+        <div style={Object.assign({
+            width: "fit-content",
+            height: "fit-content",
+        }, (horizontal &&  !end) && {
+            position: "fixed",
+            top: 0,
+            transform: `translateX(-${elementTransformX}px)`
+        }, end && {
+            transform: `translateX(-${elementEndPositionX}px) translateY(${elementEndPositionY}px)`
+        }) as any}>
+            {children}
+        </div>
+    </div>
+}
+export default ScrollHorizontal;
--- pages/future/index.tsx
+++ pages/future/index.tsx
@@ -1,72 +1,127 @@
+import { MarkGithubIcon } from "@primer/octicons-react"
 import Footer from "./components/footer"
 import Header from "./components/header"
 import ScrollClipAnimationText from "./components/scrollClipAnimationText"
+import ScrollHorizontal from "./components/scrollHorizontalv2"
 import { Char, ScrollText } from "./components/scrollText"
 import { TitleTextWithSimpleShowAnimation } from "./components/Text"
 
+const TimeLine = () => {
+
+}
+type element = React.ReactComponentElement<any>
+type elements = Array<element>
+
+const Main = ({children}:{
+    children: element | elements
+}) => {
+    return <div style={{backgroundColor: "#f8f8f8"}}>
+    <div style={{borderRadius: "0 0 64px 64px", backgroundColor: "#fff", minHeight: "calc(100vh - 250px)", paddingBottom: 64}}>
+        <div style={{
+            minHeight: "100vh"
+        }}>
+            {children}
+        </div>
+    </div>
+</div>
+}
+
+const footerTexts = [{
+    animationDuration: 8000,
+    text: "git.abc.osaka　　abc.osaka",
+    width: 180
+},{
+    text: "FOR SCHOOL RULE",
+    width: 140
+}]
+const footerLinks = [{
+    href: "https://github.com/applemango/applemango.github.io/blob/main/pages/future/index.tsx",
+    content: "このサイトのコード | source code"
+},{
+    href: "https://abc.osaka/edit/privacy-policy",
+    content: "プライバシーポリシー | Privacy Policy"
+},{
+    href: "https://forms.gle/qh1fdKDp82Q5m4ER6",
+    content: "お問い合わせフォーム | Contact Form"
+},{
+    href: "https://abc.osaka",
+    content: "メインページ | Main Page"
+}]
+
+const TextNormal = ({children, margin = false}:{
+    children: string | React.ReactElement<any>,
+    margin?: boolean
+}) => {
+    return <div>
+        <p style={{
+            fontSize: 20,
+            color: "#222",
+            margin: margin ? "initial" : `${margin} 0`
+        }}>
+            {children}
+        </p>
+    </div>
+}
+
 const Future = () => {
     return <div>
-        <div style={{backgroundColor: "#f8f8f8"}}>
-            <div style={{borderRadius: "0 0 64px 64px", backgroundColor: "#fff", minHeight: "calc(100vh - 250px)", paddingBottom: 64}}>
-                <div style={{
-                    height: "100vh"
-                }}>
-                    <Header page={{
-                        "home": "/",
-                        "future": "/future",
-                        "about": "/"
-                    }} />
-                    <TitleTextWithSimpleShowAnimation style={{
-                        fontSize: "15vw",
-                        marginBottom: 24
-                    }} text={<>
-                        <Char char={"H"} />
-                        <Char char={"e"} />
-                        <Char char={"l"} />
-                        <Char char={"l"} />
-                        <Char char={"o"} />
-                        <Char char={","} />
-                        <span> </span>
-                        <Char char={"w"} />
-                        <Char char={"o"} />
-                        <Char char={"r"} />
-                        <Char char={"l"} />
-                        <Char char={"d"} />
-                        <Char char={"!"} />
-                    </>} />
-                    <TitleTextWithSimpleShowAnimation style={{
-                        marginTop: 24
-                    }} delay={1} text={
-                        <ScrollClipAnimationText style={{
-                            fontSize: "6vw",
-                            fontWeight: 500,
-                            marginTop: 24
-                        }} text="Coming soon?" />
-                    } />
+        <Header page={{
+            "home": "/",
+            "future": "/future",
+            "about": "/"
+        }} />
+        <Main>
+            <TitleTextWithSimpleShowAnimation style={{
+                fontSize: "15vw",
+                marginBottom: 24,
+                fontWeight: 500,
+            }} text={"Hello, world!"} />
+            <TitleTextWithSimpleShowAnimation delay={.5} style={{
+                fontSize: "4vw",
+                marginBottom: 24,
+                fontWeight: 500,
+            }} text={"こんなサイト見るだけ損だよ"} />
+            <TextNormal>
+                未来のなりたい姿か、...
+                <br />
+                僕にはたった数行しか書けないスペースは少なすぎるからこのsiteに書くよ
+                <br />
+                <br/>
+                とりあえず未来を考える前にこの3年間で何をしたか見るか
+            </TextNormal>
+            <ScrollHorizontal>
+                <div>
+                    
                 </div>
-                {/*<ScrollClipAnimationText style={{fontSize: 24}} text="にしてもこのサイトどうしよう" />*/}
+            <div style={{
+                    height: "100vh",
+                    display: "flex",
+                    alignItems: "center",
+                    flexWrap: "nowrap",
+                    width: "fit-content",
+                    padding: "0 24px",
+                    backgroundColor: "#eee",
+                }}>
+                    {(new Array(10)).fill(0).map((_, i) => <div key={i} style={{
+                        height: "80%",
+                        width: 500,
+                        backgroundColor: i == 9 ? "red" : i == 0 ? "blue" : "#fff",
+                        borderRadius: "64px",
+                        marginLeft: 24,
+                    }}>
+                    </div>)}
+                </div>
+            </ScrollHorizontal>
+            <div>
+                ではまたいつか
+
+                それにしてもよく許可してくれたな、QRコード書くの
             </div>
-        </div>
-        <Footer texts={[{
-            animationDuration: 8000,
-            text: "git.abc.osaka　　abc.osaka",
-            width: 180
-        },{
-            text: "FOR SCHOOL RULE",
-            width: 140
-        }]} links={[{
-            href: "https://github.com/applemango/applemango.github.io/blob/main/pages/future/index.tsx",
-            content: "このサイトのコード | source code"
-        },{
-            href: "https://abc.osaka/edit/privacy-policy",
-            content: "プライバシーポリシー | Privacy Policy"
-        },{
-            href: "https://forms.gle/qh1fdKDp82Q5m4ER6",
-            content: "お問い合わせフォーム | Contact Form"
-        },{
-            href: "https://abc.osaka",
-            content: "メインページ | Main Page"
-        }]} />
+        </Main>
+        <Footer
+            texts={footerTexts}
+            links={footerLinks}
+        />
     </div>
 }
 export default Future
